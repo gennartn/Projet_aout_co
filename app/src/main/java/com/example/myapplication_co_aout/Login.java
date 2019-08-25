b56f6955ad85d63007af492f7a2bc714b47cfa66
Test Nicolas <nicolas.gennart@hotmail.com>
2019-08-25 18:02:00 +0200

    login

---
+++ app/src/main/java/com/example/myapplication_co_aout/Login.java
@@ -0,0 +1,74 @@
+package com.example.myapplication_co_aout;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class Login extends AppCompatActivity {
+    private static EditText username;
+    private static EditText password;
+    private static TextView identifiant;
+    private static TextView password_TextView;
+    private static Button login_btn;
+    private static Button creat_new_account;
+    private DatabaseHelper db;
+    private New_login new_login;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        username = (EditText) findViewById(R.id.username_1);
+        password = (EditText) findViewById(R.id.password_1);
+        login_btn= (Button) findViewById(R.id.button_login);
+        creat_new_account = (Button) findViewById(R.id.button_creat_new_account);
+
+        db = new_login.getDbLogin();
+
+        creat_new_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Open_New_Login();
+            }
+        });
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Check_Data_Base(username.getText().toString(),password.getText().toString());
+            }
+        });
+
+    }
+
+    public void Open_New_Login(){
+        Intent intent = new Intent(this, New_login.class);
+        startActivity(intent);
+    }
+
+    public void Check_Data_Base(String username, String password){
+
+        if(db == null){
+            Toast.makeText(Login.this, "Aucun compte n'a été créé", Toast.LENGTH_LONG).show();
+        }
+        else{
+            if(db.checkData(username,password)){
+                Toast.makeText(Login.this, "Votre code est correct", Toast.LENGTH_LONG).show();
+            }
+            else{
+                Toast.makeText(Login.this, "Votre code est incorrect", Toast.LENGTH_LONG).show();
+            }
+        }
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication_co_aout/New_login.java
@@ -0,0 +1,89 @@
+package com.example.myapplication_co_aout;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class New_login extends Login {
+    private static TextView user_name_1;
+    private static TextView password_1;
+    private static TextView confirm_password;
+    private static Button creat_btn;
+    private static EditText username;
+    private static EditText password;
+    private static EditText password_confirm;
+    private static Button  return_1;
+    public DatabaseHelper db;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_login);
+
+        creat_btn = (Button) findViewById(R.id.button_creat);
+        return_1 = (Button) findViewById(R.id.button_return);
+
+        username = (EditText) findViewById(R.id.username);
+
+        password = (EditText) findViewById(R.id.password_1);
+
+        password_confirm = (EditText) findViewById(R.id.password_confirm);
+
+        return_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Open_Login();
+            }
+        });
+
+        if(CompareString(password.getText().toString(), password_confirm.getText().toString())){
+            creat_btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    addData(password.getText().toString(), password_confirm.getText().toString());
+                }
+            });
+        }
+        else{
+            Toast.makeText(New_login.this, "la confirmation du password est fausse", Toast.LENGTH_LONG).show();
+            Toast.makeText(New_login.this, "retapez la confirmation du password", Toast.LENGTH_LONG).show();
+        }
+
+
+    }
+    public void Open_Login(){
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+    }
+
+    public boolean CompareString(String password, String password_confirm){
+        if(password.equals(password_confirm)){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
+    public DatabaseHelper getDbLogin(){
+        return db;
+    }
+
+    public void addData(String username, String password) {
+        boolean isInserted = db.insertData(username,password);
+        if(isInserted == true){
+            Toast.makeText(New_login.this, "Votre account est créé", Toast.LENGTH_LONG).show();
+        }
+        else{
+            Toast.makeText(New_login.this, "Problème pour créer votre account", Toast.LENGTH_LONG).show();
+        }
+
+    }
+}
